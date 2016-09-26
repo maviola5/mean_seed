c404cc7a1133b9c2873a8375fc863ecac8ae6303
Tony V <maviola5@gmail.com>
2016-09-26 17:42:38 -0400

    added Helmet.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var helmet = require('helmet');
 
 var api = require('./api/routes/index');
 var users = require('./api/routes/users');
@@ -12,6 +13,7 @@ var app = express();
 
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(helmet())
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
